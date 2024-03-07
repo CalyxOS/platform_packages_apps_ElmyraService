722f257844e16831a095388310fd1740efb28ef0
Aayush Gupta <aayushgupta219@gmail.com>
2024-03-07 22:54:41 +0530

    Handle null-safety errors for AOSP QPR2
    Change-Id: I63a5a872d6f53d716d3796fc780b04ec17cc6b90
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>

--- src/org/protonaosp/elmyra/settings/SummaryProvider.kt
+++ src/org/protonaosp/elmyra/settings/SummaryProvider.kt
@@ -33,17 +33,20 @@ class SummaryProvider : ContentProvider() {
     private lateinit var prefs: SharedPreferences
 
     override fun onCreate(): Boolean {
-        prefs = context.getDePrefs()
+        prefs = requireContext().getDePrefs()
         return true
     }
 
-    override fun call(method: String, uri: String, extras: Bundle?): Bundle {
+    override fun call(method: String, arg: String?, extras: Bundle?): Bundle? {
         val bundle = Bundle()
         val summary = when (method) {
-            "entry" -> if (prefs.getEnabled(context)) {
-                context.getString(R.string.settings_entry_summary_on, prefs.getActionName(context))
+            "entry" -> if (prefs.getEnabled(requireContext())) {
+                requireContext().getString(
+                    R.string.settings_entry_summary_on,
+                    prefs.getActionName(requireContext())
+                )
             } else {
-                context.getString(R.string.settings_entry_summary_off)
+                requireContext().getString(R.string.settings_entry_summary_off)
             }
             else -> throw IllegalArgumentException("Unknown method: $method")
         }
